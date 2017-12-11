32fe55e54aa88a0bc8a31fc9fe863195fd1eb54a
Damien Pijaczynski <dpijaczynski@tungstensoftware.fr>
2017-12-11 09:57:49 +0100

    Fix babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -31,16 +31,7 @@ var config = {
         }),
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendor',
-            minChunks: function (module, count) {
-                // any required modules inside node_modules are extracted to vendor
-                return (
-                    module.resource &&
-                    /\.js$/.test(module.resource) &&
-                    module.resource.indexOf(
-                        path.join(__dirname, "node_modules")
-                    ) === 0
-                )
-            }
+            minChunks: ({ resource }) => /node_modules/.test(resource),
         }),
     ],
 };
